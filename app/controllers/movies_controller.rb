020c3bbac79d3be53e153502a5c6809472a4aa57
Jim Knight <jim.knight@lavatech.com>
2013-12-24 16:14:12 -0500

    get json call at least working for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,15 @@
 class MoviesController < ApplicationController
 
+  require 'open-uri'
+  require 'json'
   def list
-
+    redbox_api_key = ENV["REDBOX_API_KEY"]
+    url = "https://api.redbox.com/v3/inventory/stores/postalcode/20176?apiKey=#{redbox_api_key}&retailer=giant&count=1"
+    # request = open(url)
+    # binding.pry
+    # result = Nokogiri.XML(open(url).read)
+    result = JSON.parse(open(url,"Accept" => "application/json").read)
+    binding.pry
   end
 
 end
