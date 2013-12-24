51017966ed25c51012224a82468e6f29d1f03c6d
Jim Knight <jim.knight@lavatech.com>
2013-12-24 17:26:47 -0500

    pretty output to start with - sort of

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,14 +10,16 @@ class MoviesController < ApplicationController
     store_api = "https://api.redbox.com/v3/stores?apiKey=#{redbox_api_key}&storeList=#{store_id}"
     store_json = JSON.parse(open(store_api,"Accept" => "application/json").read)
     product_info = []
+    max_loops = 1
     result["Inventory"]["StoreInventory"][1]["ProductInventory"].each do |product|
-      if product["@inventoryStatus"] == "InStock"
+      if product["@inventoryStatus"] == "InStock" && max_loops > 0
         product_api = "https://api.redbox.com/v3/products?apiKey=#{redbox_api_key}&productIds=#{product["@productId"]}"
         product_json = JSON.parse(open(product_api,"Accept" => "application/json").read)
         product_info << product_json
+        max_loops = max_loops - 1
       end
     end
-    @result = product_info
+    @products = product_info
   end
 
 end
