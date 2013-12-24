99791d2495fbd6e488f2bfee5cecd1570206ea01
Jim Knight <jim.knight@lavatech.com>
2013-12-24 16:34:58 -0500

    get all the inventory product id's raw for local giant

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,12 +4,18 @@ class MoviesController < ApplicationController
   require 'json'
   def list
     redbox_api_key = ENV["REDBOX_API_KEY"]
-    url = "https://api.redbox.com/v3/inventory/stores/postalcode/20176?apiKey=#{redbox_api_key}&retailer=giant&count=1"
-    # request = open(url)
-    # binding.pry
-    # result = Nokogiri.XML(open(url).read)
+    url = "https://api.redbox.com/v3/inventory/stores/postalcode/20176?apiKey=#{redbox_api_key}&retailer=giant&count=2"
     result = JSON.parse(open(url,"Accept" => "application/json").read)
-    binding.pry
+    store_id = result["Inventory"]["StoreInventory"][1]["@storeId"] # second giant store near us
+    store_api = "https://api.redbox.com/v3/stores?apiKey=#{redbox_api_key}&storeList=#{store_id}"
+    store_json = JSON.parse(open(store_api,"Accept" => "application/json").read)
+    product_ids = []
+    result["Inventory"]["StoreInventory"][1]["ProductInventory"].each do |product|
+      if product["@inventoryStatus"] == "InStock"
+        product_ids << product["@productId"]
+      end
+    end
+    @result = product_ids
   end
 
 end
