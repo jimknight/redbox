0937e1bac77630250a0f90a57d03481dcb6f9007
Jim Knight <jim.knight@lavatech.com>
2013-12-24 19:40:37 -0500

    start on rotten tomatoes api

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,6 +4,7 @@ class MoviesController < ApplicationController
   require 'json'
   def list
     redbox_api_key = ENV["REDBOX_API_KEY"]
+    rt_api_key = ENV["ROTTEN_TOMATOES_API_KEY"]
     url = "https://api.redbox.com/v3/inventory/stores/postalcode/20176?apiKey=#{redbox_api_key}&retailer=giant&count=2"
     result = JSON.parse(open(url,"Accept" => "application/json").read)
     store_id = result["Inventory"]["StoreInventory"][1]["@storeId"] # second giant store near us
@@ -17,6 +18,10 @@ class MoviesController < ApplicationController
         product_json = JSON.parse(open(product_api,"Accept" => "application/json").read)
         product_info << product_json
         max_loops = max_loops - 1
+        movie_title = product_json["Products"]["Movie"]["Title"].gsub(" ","+")
+        rt_api = "http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=#{rt_api_key}&q=#{movie_title}&page_limit=1"
+        rt_json = JSON.parse(open(rt_api,"Accept" => "application/json").read)
+        binding.pry
       end
     end
     @products = product_info
