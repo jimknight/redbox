c1657e3657e0d89ecfd7f3f4680ca840b7a99fad
Jim Knight <jim.knight@lavatech.com>
2013-12-31 10:13:11 -0500

    added root pointer to movies

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Redbox::Application.routes.draw do
   resources :products
 
-  get 'movies/list' => 'movies#list'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'products#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
