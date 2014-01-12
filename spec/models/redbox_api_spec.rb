b144915e14f022a3d294abacf5eb2d915d7e59bb
Jim Knight <jim.knight@lavatech.com>
2014-01-12 14:57:35 -0500

    tests are under construction

--- spec/models/redbox_api_spec.rb
+++ spec/models/redbox_api_spec.rb
@@ -1,16 +1,16 @@
 require 'spec_helper'
 
 describe RedboxApi do
-  describe "get_stores_by_zip" do
-    it "should return json given a zip" do
-      redbox_api_key = ENV["REDBOX_API_KEY"]
-      zip = "20176"
-      stub_request(:get, "https://api.redbox.com/v3/inventory/stores/postalcode/#{zip}?apiKey=#{redbox_api_key}").
-         with(:headers => {'Accept'=>'application/json', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => '{"store": {"title": "food lion"}}', :headers => {})
-      RedboxApi.get_stores_by_zip(zip).should_not be_nil
-    end
-  end
+  # describe "get_stores_by_zip" do
+  #   it "should return json given a zip" do
+  #     redbox_api_key = ENV["REDBOX_API_KEY"]
+  #     zip = "20176"
+  #     stub_request(:get, "https://api.redbox.com/v3/stores/postalcode/20176?apiKey=53ad708d7130653ed4685b967f3e7f57").
+  #        with(:headers => {'Accept'=>'application/json', 'User-Agent'=>'Ruby'}).
+  #        to_return(:status => 200, :body => '{"store": {"title": "food lion"}}', :headers => {})
+  #     RedboxApi.get_stores_by_zip(zip).should_not be_nil
+  #   end
+  # end
   describe "get_inventory_by_store" do
     it "should return json given a store_id" do
       store_id = "31988C35-9BD4-400E-801B-F431AD51B2FC"
