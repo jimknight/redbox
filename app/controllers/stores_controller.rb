231825196543c1bcd35664c8b28daab8fe7e17ca
Jim Knight <jim.knight@lavatech.com>
2014-01-13 20:24:51 -0500

    order store by name

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
 
   def index
-    @stores = Store.all
+    @stores = Store.order(:store_name)
   end
 
   # before_action :set_store, only: [:show, :edit, :update, :destroy]
